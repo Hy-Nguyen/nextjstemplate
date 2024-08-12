4236a71f2f50ed292cf4e7e9ee7056fe41a01ea6
Hy Nguyen <giahy316@gmail.com>
2024-08-12 15:39:16 -0700

    updated layout

---
+++ components/layout/LayoutWrapper.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { Toaster } from 'react-hot-toast';
+
+export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
+}
